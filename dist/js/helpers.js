33ddd1761bd43c426dc9459d7ea01e5e44465e21
tyran0 <119806764+tyran0@users.noreply.github.com>
2023-02-06 16:04:20 +0400

    Finished with progress bar

--- dist/js/helpers.js
+++ dist/js/helpers.js
@@ -1,5 +1,36 @@
+export function keyToMethod(key) {
+    if (typeof key !== 'string') return;
+    
+    const firstLetter = key.at(0);
+    if (firstLetter !== firstLetter.toUpperCase()) {
+        key = key.replace(firstLetter, firstLetter.toUpperCase());
+    }
+
+    const underscorePos = key.indexOf('_');
+    if (underscorePos !== -1) {
+        const pairLetterPos = underscorePos + 1; 
+        const toReplace = '_' + key.at(pairLetterPos);
+        const replaceWith = key.at(pairLetterPos).toUpperCase();
+        const tempKey = key.replace(toReplace, replaceWith);
+        key = keyToMethod(tempKey);
+    }
+
+    return key;
+}
+
+export default function PrettyNumber() {
+    this.prettify = function(input) {
+        const output = '1,024';
+        return output;
+    }
+    this.unprettify = function(input) {
+        const output = Number(input.replace(',', ''));
+        return output;
+    }
+}
+
 export function assert(object, type) {
-    if (!object) return false;
+    if (!object) return;
 
     const isFunction = (typeof object === 'function');
     const isPrimitive = !(object instanceof Object);
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,14 @@
-import { assert, isButton } from "./helpers.js";
+import PrettyNumber from "./helpers.js";
+import { assert, isButton, keyToMethod } from "./helpers.js";
 
 if (document.readyState === 'interactive') {
     const project = new Project(document.querySelector('#project'));
 
     project.addButtonHandler('#projectInfo');
     project.addButtonHandler('#projectDesc');
+
+    project.addProgressHandler('#projectStats');
+    project.updateProgress();
 
     const rewardSelectionModal = document.querySelector('#rewardSelectionModal');
     project.addModalHandler('selectReward', new ModalHandler(rewardSelectionModal));
@@ -13,11 +17,36 @@ if (document.readyState === 'interactive') {
 function Project(node) {
     assert(node, Element);
     this.node = node;
-    this.stats = node
-        .querySelector('#projectStats');
+    this.statistics = {};
 
     this.bookmark = function(pressedButton) {
         console.log('Boomarked this project!', pressedButton);
+    }
+
+    this.updateProgress = function() {
+        const pNum = new PrettyNumber();
+        const value = pNum.unprettify(
+            this.statistics.getBackingCurrent());
+        const maxValue = pNum.unprettify( 
+            this.statistics.getBackingTotal());
+
+        const percentage = (value * 100) / maxValue;
+        this.statistics.setProgress(percentage);
+    }
+
+    this.addProgressHandler = function(selector) {
+        const node = this.node.querySelector(selector);
+        const valueHolders =  node.querySelectorAll('[data-value]');
+
+        Array.from(valueHolders).forEach(node => {
+            const attr = (node.tagName === 'PROGRESS')
+                ? 'value' : 'innerText';
+            const key = node.dataset.value;
+            const methodName = keyToMethod(key);
+
+            this.statistics['get' + methodName] = () => node[attr];
+            this.statistics['set' + methodName] = value => node[attr] = value;
+        });
     }
 
     this.addModalHandler = function(name, handler) {
@@ -25,7 +54,7 @@ function Project(node) {
     }
 
     this.addButtonHandler = function(selector) {
-        const node = document.querySelector(selector);
+        const node = this.node.querySelector(selector);
         node.addEventListener('click', e => {
             const target = e.target;
             const button = isButton(target);
